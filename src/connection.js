d9272fb753760cefe9bc7423f6962f91adee66f6
CarlosPerez92 <carlos92perez1@gmail.com>
2020-09-17 02:58:07 -0700

    first commit

---
+++ src/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+require("dotenv").config({path:'variables.env'})
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(db => console.log('Connection establishe successfully')) 
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const app = require('./app')
+require('./connection');
+require("dotenv").config({path:'variables.env'})
+
+//leer variables de sesioon
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+async function init() {
+    await app.listen(port);
+    console.log('Connected correctly to server')
+}
+
+init();
